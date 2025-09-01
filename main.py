527c5f2e660fbb4b7dff4b61c174f3b063f61558
Alexey Lunev <cheembox573@gmail.com>
2025-09-01 15:09:37 +0000

    customize delay

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import argparse
 parser = argparse.ArgumentParser()
 
 parser.add_argument('-i', '--id', type=int, default=-1001373811109)
+parser.add_argument('-d', '--delay', type=int, default=30)
 parser.add_argument('--api-id', type=int)
 parser.add_argument('--api-hash', type=str)
 
@@ -12,10 +13,10 @@ args = parser.parse_args()
 import asyncio
 import telethon
 
-async def main(client, chat):
+async def main(client, chat, delay):
     while True:
         async with client.action(chat, 'typing'):
-            await asyncio.sleep(30)
+            await asyncio.sleep(delay)
 
 with telethon.TelegramClient('septemba', args.api_id, args.api_hash) as client:
-    client.loop.run_until_complete(main(client, args.id))
+    client.loop.run_until_complete(main(client, args.id, args.delay))
